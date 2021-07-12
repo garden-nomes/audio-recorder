a4fe928cc20e682f4f7127801db3be48c1648836
nomes <noahbweiner@gmail.com>
2021-07-12 13:20:56 -0700

    client: record audio

--- client/src/components/Recorder.vue
+++ client/src/components/Recorder.vue
@@ -17,23 +17,63 @@ import RecordTimer from "./RecordTimer.vue";
 
 export default defineComponent({
   name: "Recorder",
+
   components: { RecordButton, RecordTimer },
+
   data() {
     return {
       isRecording: false,
       startTime: null as Date | null,
       stopTime: null as Date | null,
+      chunks: [] as Blob[],
+      recorder: null as MediaRecorder | null,
     };
   },
+
+  async mounted() {
+    // create a media recorder (will prompt the user for permissions)
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+
+        this.recorder = new MediaRecorder(stream);
+        this.recorder.ondataavailable = (event) => this.chunks.push(event.data);
+        this.recorder.onstop = this.onRecordingStopped;
+      } catch (err) {
+        console.error(
+          `unable to create media recorder:\n\n${err.stack || err}`
+        );
+      }
+    } else {
+      console.error("sorry, media recording isn't supported on this browser");
+    }
+  },
+
   methods: {
     startRecording() {
-      this.isRecording = true;
-      this.startTime = new Date();
-      this.stopTime = null;
+      if (this.recorder !== null) {
+        this.isRecording = true;
+        this.startTime = new Date();
+        this.stopTime = null;
+        this.recorder.start();
+      }
     },
+
     stopRecording() {
-      this.isRecording = false;
-      this.stopTime = new Date();
+      if (this.recorder !== null) {
+        this.isRecording = false;
+        this.stopTime = new Date();
+
+        this.recorder.stop();
+      }
+    },
+
+    onRecordingStopped() {
+      console.log(this.chunks.length);
+      this.chunks = [];
     },
   },
 });
