9e95d7358b63cba20ec2ae9047778ceb4e452a26
nomes <noahbweiner@gmail.com>
2021-07-13 10:28:52 -0700

    server: save audio files with human-readable names

--- server/src/audio-storage.ts
+++ server/src/audio-storage.ts
@@ -1,13 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import { hri } from 'human-readable-ids';
 
 export default class AudioStorage {
     constructor(private dir: string) {}
 
     async save(audioData: Buffer) {
-        // note: it's possible (but unlikely for our purposes) for naming collisions to occur,
-        // in which case the previous audio file will be overwritten
-        const filename = path.join(this.dir, `${Date.now()}.ogg`);
+        const filename = path.join(this.dir, `${hri.random()}.ogg`);
 
         await new Promise<void>((resolve, reject) => {
             fs.writeFile(filename, audioData, (err) => {
---
+++ server/src/human-readable-ids.d.ts
@@ -0,0 +1,3 @@
+declare module 'human-readable-ids' {
+    export const hri: { random(): string };
+}
